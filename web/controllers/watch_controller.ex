a6aaed15a90fda9a8738e312279cc1f472fb6537
= <steiner.mischa@gmail.com>
2016-10-25 22:44:52 +0900

    fixed watch show handling for logged in and non-logged in users

--- web/controllers/watch_controller.ex
+++ web/controllers/watch_controller.ex
@@ -4,29 +4,30 @@ defmodule Flashklip.WatchController do
   alias Flashklip.Metavideo
   alias Flashklip.Video
 
-  def show(conn, %{"id" => id}) do
-
-    case is_map(conn.assigns.current_user) && @own_video == true do
-      true ->
-        video =
-          Repo.get!(Video, id)
-          |> Repo.preload(:metavideo)
-        render conn, "show.html", video: video.metavideo, user_video: video
-      _ ->
-        metavideo =
-          Repo.get!(Metavideo, id)
-          |> Repo.preload(:videos)
-
-        metavideo_klips = metavideo.videos |> Repo.preload(:klips)
-
-        klips =
-          Enum.flat_map(metavideo_klips, fn(v) ->
-            v.klips
-            |> Repo.preload(:user) end)
-            |> Enum.sort()
-
-        render conn, "show.html", video: metavideo, user_video: metavideo, klips: klips
+  def show(conn, %{"id" => id, "v" => v}) do
+    if String.to_integer(v) > 0 do
+      video =
+        Repo.get!(Video, id)
+        |> Repo.preload(:metavideo)
+      render conn, "show.html", video: video.metavideo, user_video: video, show: ""
+    else
+      metavideo =
+        Repo.get!(Metavideo, id)
+        |> Repo.preload(:videos)
+      metavideo_klips = metavideo.videos |> Repo.preload(:klips)
+      klips =
+        Enum.flat_map(metavideo_klips, fn(v) ->
+          v.klips
+          |> Repo.preload(:user) end)
+          |> Enum.sort()
+      show =
+        case is_map(conn.assigns.current_user) do
+          true ->
+            ""
+          _ ->
+            "hide"
+        end
+      render conn, "show.html", video: metavideo, user_video: metavideo, klips: klips, show: show
     end
-
   end
 end
