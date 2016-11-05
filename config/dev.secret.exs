320c5fd309e942db42cf75527d229b5d01fac67a
= <steiner.mischa@gmail.com>
2016-11-05 22:50:49 +0900

    prepare merge from pwless branch

---
+++ config/dev.secret.exs
@@ -0,0 +1,6 @@
+use Mix.Config
+
+config: :Flashklip,
+  mailgun_domain: "https://api.mailgun.net/v3/sandbox4b323e6043014043a66a753c03dee035.mailgun.org",
+  mailgun_key: "key-7b853c6aed26bb87539ad3ae783d2b7a"
+
