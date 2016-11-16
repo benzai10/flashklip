cc704490cf0efa7064c211f4c8ea0be973630bb8
= <steiner.mischa@gmail.com>
2016-11-16 10:04:38 +0900

    https for youtube player

--- web/static/js/player.js
+++ web/static/js/player.js
@@ -7,7 +7,7 @@ let Player = {
       this.onIframeReady(domId, playerId, onReady)
     }
     let youtubeScriptTag = document.createElement("script")
-    youtubeScriptTag.src = "//www.youtube.com/iframe_api"
+    youtubeScriptTag.src = "https://www.youtube.com/iframe_api"
     document.head.appendChild(youtubeScriptTag)
   },
 
