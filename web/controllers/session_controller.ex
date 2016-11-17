6e741f0d8958f891defcc7d5833f834fbdba775c
= <steiner.mischa@gmail.com>
2016-11-17 12:26:02 +0900

    fix orbit height

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -55,28 +55,4 @@ defmodule Flashklip.SessionController do
     |> redirect(to: page_path(conn, :index))
   end
 
-
-  ### classic auth
-  # def new(conn, _) do
-  #   render conn, "new.html"
-  # end
-
-  # def create(conn, %{"session" => %{"username" => user, "password" => pass}}) do
-  #   case Flashklip.Auth.login_by_username_and_pass(conn, user, pass, repo: Repo) do
-  #     {:ok, conn} ->
-  #       conn
-  #       |> put_flash(:info, "Welcome back!")
-  #       |> redirect(to: video_path(conn, :index))
-  #     {:error, _reason, conn} ->
-  #       conn
-  #       |> put_flash(:error, "Invalid username/password combination")
-  #       |> render("new.html")
-  #   end
-  # end
-
-  # def delete(conn, _) do
-  #   conn
-  #   |> Flashklip.Auth.logout()
-  #   |> redirect(to: page_path(conn, :index))
-  # end
 end
