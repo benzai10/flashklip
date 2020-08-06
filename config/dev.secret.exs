15689acf2e44cfebc8497f4b9374d298c0d40d41
benzai10 <mischa@benzai10.com>
2020-08-06 13:53:38 +0200

    obfuscate various keys

--- config/dev.secret.exs
+++ config/dev.secret.exs
@@ -2,8 +2,8 @@ use Mix.Config
 
 config :flashklip,
   mailgun_domain: "https://api.mailgun.net/v3/flashklip.com",
-  mailgun_key: "key-7b853c6aed26bb87539ad3ae783d2b7a",
-  youtube_api_key: "AIzaSyDTeV8UtwCWOXATwMrlOvZf0id4On_O4Qc",
-  letsencrypt_key: "Dr2RygtF8x47ck-0WssZqcnhMKuTbmf8TZGoQSPrDT8"
+  mailgun_key: MAILGUN_KEY,
+  youtube_api_key: YOUTUBE_API_KEY,
+  letsencrypt_key: LETS_ENCRYPT_KEY
 
 
